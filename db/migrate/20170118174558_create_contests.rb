2c0f895a53408fcb6c5274cad55b4b9b2b1713cc
MaxMEllon <maxmellon1994@gmail.com>
2017-01-19 04:05:00 +0900

    :sparkles: generate content model

---
+++ db/migrate/20170118174558_create_contests.rb
@@ -0,0 +1,11 @@
+class CreateContests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contests do |t|
+      t.datetime :start_at, null: false
+      t.datetime :end_at, null: false
+      t.boolean :is_active, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
