9f18292fd790db80ecede67c102a4e3d050aa14f
MaxMEllon <maxmellon1994@gmail.com>
2017-01-18 15:53:09 +0900

    :white_check_mark: Add specs of TopController

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -2,4 +2,10 @@
 require 'rails_helper'
 
 RSpec.describe TopController, type: :controller do
+  describe 'GET #inquiry' do
+    it 'returns http success' do
+      get :inquiry
+      expect(response).to have_http_status(200)
+    end
+  end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'rspec/retry'
+require 'rubocop'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.use_instantiated_fixtures = false
+  config.mock_with :rspec
+
+  config.verbose_retry = true
+  config.display_try_failure_messages = true
+
+  config.include Devise::TestHelpers, type: :controller
+
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
+  end
+  config.infer_spec_type_from_file_location!
+  config.raise_errors_for_deprecations!
+
+  config.before(:suite) do
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.example_status_persistence_file_path = 'examples.txt'
+end
+
+ActiveRecord::Migration.maintain_test_schema!
