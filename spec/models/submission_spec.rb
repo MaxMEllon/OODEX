3150bf37f0429a1d3b3258274c7934888bc5ef2e
MaxMEllon <maxmellon1994@gmail.com>
2017-01-24 06:28:17 +0900

    :shirt: annotate

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
+# == Schema Information
+#
+# Table name: submissions
+#
+#  code        :text             not null
+#  created_at  :datetime         not null
+#  id          :integer          not null, primary key
+#  player_id   :integer
+#  question_id :integer
+#  status      :integer          not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_submissions_on_player_id    (player_id)
+#  index_submissions_on_question_id  (question_id)
+#
+
 require 'rails_helper'
 
 RSpec.describe Submission, type: :model do
