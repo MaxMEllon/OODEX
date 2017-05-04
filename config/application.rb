e7069a49d2b3bcc52f1987d08ef64a3dda85c43e
MaxMEllon <maxmellon1994@gmail.com>
2017-05-04 20:54:21 +0900

    CI

--- config/application.rb
+++ config/application.rb
@@ -16,18 +16,21 @@ require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load if Rails.env != 'production'
+Dotenv::Railtie.load if Rails.env != production
 
-class TEE::Application < Rails::Application
-  config.time_zone = 'Tokyo'
-  config.active_record.default_timezone = :local
-  config.active_record.time_zone_aware_types = %i[datetime time]
-  config.sass.preferred_syntax = :sass
-  config.sass.cache = true
-  config.i18n.default_locale = :ja
+module TEE
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_types = %i[datetime time]
+    config.assets.initialize_on_precompile = false
+    config.sass.preferred_syntax = :sass
+    config.sass.cache = true
+    config.i18n.default_locale = :ja
 
-  config.generators do |g|
-    g.helper false
-    g.assets false
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
   end
 end
